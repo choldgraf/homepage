709649830bc111a77f523af0317745d9acacf980
YuviPanda <yuvipanda@gmail.com>
2021-04-23 13:37:16 +0530

    Add JSON Schema validation for our config

--- build.py
+++ build.py
@@ -2,6 +2,26 @@ import jinja2
 import os
 import shutil
 import subprocess
+import jsonschema
+from ruamel.yaml import YAML
+
+yaml = YAML(typ="safe")
+
+
+def validate():
+    """
+    Validate hubs.yaml config
+
+    Prevents runtime bugs
+    """
+    schema_file = "src/schema.yaml"
+    config_file = "src/hubs.yaml"
+    with open(config_file) as cf, open(schema_file) as sf:
+        cluster_config = yaml.load(cf)
+        schema = yaml.load(sf)
+        # Raises useful exception if validation fails
+        jsonschema.validate(cluster_config, schema)
+
 
 env = jinja2.Environment(loader=jinja2.FileSystemLoader("templates"))
 
@@ -10,6 +30,7 @@ def static_url(url):
     return url
 
 
+validate()
 params = {"static_url": static_url, "authenticator_login_url": "https://some-login-url"}
 
 os.makedirs("_build", exist_ok=True)

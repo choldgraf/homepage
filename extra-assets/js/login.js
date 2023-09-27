f427c3ccfbd20b9b10ed592c9cc073998c3ee3b8
Hanbyul Jo <hanbyul@nyu.edu>
2023-09-27 13:51:07 -0400

    Use more specific id

--- extra-assets/js/login.js
+++ extra-assets/js/login.js
@@ -18,10 +18,10 @@ function showDiscalimerModal() {
     var HUB_MODAL_STORAGE_KEY = 'humModalDismissed';
     var showModal = localStorage.getItem(HUB_MODAL_STORAGE_KEY) === null? true: !(localStorage.getItem(HUB_MODAL_STORAGE_KEY) === 'true');
   
-    if (showModal) $('#modal').modal('show');
+    if (showModal) $('#disclaimer-modal').modal('show');
   
-    $('#modal').on('hidden.bs.modal', function (e) {
-      var doNotShowNextTime = $('#modalCheckBox')[0].checked;
+    $('#disclaimer-modal').on('hidden.bs.modal', function (e) {
+      var doNotShowNextTime = $('#disclaimer-modal-checkbox')[0].checked;
       localStorage.setItem(HUB_MODAL_STORAGE_KEY, doNotShowNextTime);
     })
 }

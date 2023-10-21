a99c7d8e2777550fa535cc92f29ba71522d18f7e
YuviPanda <yuvipanda@gmail.com>
2023-10-21 11:56:02 +0530

    Fix login sending users to 'undefined'
    - Remove code for interface selector, which is unused. Interaction
      between the modal dialog and this is probably the cause of
      users logging in being sent to an 'undefined' page
    - Remove unneeded redirectIfNeeded
    - Cleanup the JS and HTML code so further modification by others
      is easier! GHG can have as customized a front page as it wants

--- extra-assets/js/login.js
+++ extra-assets/js/login.js
@@ -1,19 +1,3 @@
-function redirectIfNeeded() {
-    // Only redirect utoronto.2i2c.cloud, lets us keep staging at staging.utoronto.2i2c.cloud
-    if (window.location.hostname === 'utoronto.2i2c.cloud') {
-        // Let's give users an indication that something is happening
-        document.write("Redirecting you to jupyter.utoronto.ca");
-        window.location.hostname = 'jupyter.utoronto.ca';
-    }
-}
-function setInterface(interfaceUrl) {
-    let loginUrl = new URL($('#home').data('authenticator-login-url'), document.location.origin);
-    loginUrl.searchParams.set('next', '/hub/user-redirect/' + interfaceUrl)
-    $('#login-button').attr(
-        'href',
-        loginUrl.toString()
-    );
-}
 function showDiscalimerModal() {
     var HUB_MODAL_STORAGE_KEY = 'humModalDismissed';
     var showModal = localStorage.getItem(HUB_MODAL_STORAGE_KEY) === null? true: !(localStorage.getItem(HUB_MODAL_STORAGE_KEY) === 'true');
@@ -26,19 +10,6 @@ function showDiscalimerModal() {
     })
 }
 $(function() {
-    redirectIfNeeded();
-    // if next query param is presentm just do nothing
-    const nextPresent = new URL(document.location).searchParams.get('next');
-    // /hub/ being next should be treated same as no next present
-    if (!nextPresent || nextPresent === "/hub/") {
-        setInterface($("input[name='interface']:checked").val());
-
-        $("input[name='interface']").change(function() {
-            if (this.checked) {
-                setInterface(this.value)
-            }
-        });
-    }
     /* show disclaimer modal */
     showDiscalimerModal()
 })
